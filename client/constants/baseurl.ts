c7ac549f31b09de5610c6790f02250515614e352
BisekhKarki <karkidai79@gmail.com>
2025-04-14 11:33:19 +0545

    Updated

--- client/constants/baseurl.ts
+++ client/constants/baseurl.ts
@@ -1,7 +1,9 @@
 import io from "socket.io-client";
 
-export const baseUrl = process.env.NEXT_BACKEND_URL || "http://localhost:4000/";
+export const baseUrl =
+  (process.env.NEXT_BACKEND_URL as string) || "http://localhost:4000/";
 
-const backendUrl = process.env.NEXT_BACKEND_URL || "http://localhost:4000";
+const backendUrl =
+  (process.env.NEXT_BACKEND_SOCKET_URL as string) || "http://localhost:4000";
 
 export const socket = io(backendUrl);
