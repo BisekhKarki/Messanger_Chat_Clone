8928b00657ddba540b90e316fd13ce19bc40f1f6
BisekhKarki <karkidai79@gmail.com>
2025-04-14 11:45:07 +0545

    Updated

--- backend/server.js
+++ backend/server.js
@@ -15,27 +15,27 @@ const PORT = process.env.PORT || 5000;
 // Server for the socket
 // const socketIo = require("socket.io");
 
+const corsOptions = {
+  origin: [
+    process.env.FRONTEND_URL,
+    "http://localhost:3000",
+    // Add any other allowed origins here
+  ],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+};
+
 const { Server } = require("socket.io");
 const http = require("http");
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: [process.env.FRONTENDURL, "http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-  },
+  cors: corsOptions,
 });
 
 // App server
 app.use(express.json());
-app.use(
-  cors({
-    origin: [process.env.FRONTENDURL, "http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 
 // Database connection
 database();
